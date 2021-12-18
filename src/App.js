76160c85764eedaccec2aac96a7f673e7de2722f
aminespon <89251553+aminespon@users.noreply.github.com>
2021-12-18 20:24:38 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import UserList from './components/UserList';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='titlea'>
+    <h1>this is my list</h1>
+    </div>
+
+      <UserList/>
+    </div>
+  );
+}
+
+export default App;
